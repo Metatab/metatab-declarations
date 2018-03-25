ebfa15ce77c32e2190aad52d09ad1edfa893d1cb
Eric Busboom <eric@civicknowledge.com>
2018-03-25 07:58:29 -0700

    More resource issues

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='metatabdecl',
-    version='1.0',
+    version='1.1',
     packages=['metatabdecl'],
     package_data={'metatabdecl': ['*.csv','*.json']},
     url='https://github.com/CivicKnowledge/metatab-declarations',
